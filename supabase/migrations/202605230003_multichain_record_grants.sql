7d07c6d61321d1bf6da89b052b2c1517f600e88f
henrysammarfo <jasonneil4040@gmail.com>
2026-05-23 09:25:20 +0100

    Grant Supabase access for multichain records

---
+++ supabase/migrations/202605230003_multichain_record_grants.sql
@@ -0,0 +1,15 @@
+grant select, insert, update, delete
+on public.arcpay_somnia_records
+to service_role;
+
+grant select, insert, update, delete
+on public.arcpay_sui_records
+to service_role;
+
+grant select, insert, update, delete
+on public.arcpay_mantle_records
+to service_role;
+
+grant select, insert, update, delete
+on public.arcpay_arbitrum_records
+to service_role;
